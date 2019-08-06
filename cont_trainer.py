34208db2e8d7b56c98bb73baf6d41d98882c9392
DQle38 <ldk308@skku.edu>
2019-08-06 16:34:33 +0900

    init warning corrected

--- cont_trainer.py
+++ cont_trainer.py
@@ -17,7 +17,7 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         # m.weight.data.normal_(0.0, 0.02)
-        nn.init.kaiming_normal(m.weight)
+        nn.init.kaiming_normal_(m.weight)
     elif classname.find('BatchNorm') != -1:
         # Estimated variance, must be around 1
         m.weight.data.normal_(1.0, 0.02)
