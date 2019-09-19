69df44ca75824fc6e26510280f1fbef1eab1c744
DQle38 <ldk308@skku.edu>
2019-09-19 18:46:57 +0900

    model init way changed

--- models/baye_cnn.py
+++ models/baye_cnn.py
@@ -1,6 +1,62 @@
 import torch.nn as nn
+import torch.nn.functional as F
 from models.bayes_layer import BayesianConv2D, BayesianLinear
 
+
+class Dcnn(nn.Module):
+    def __init__(self, input_channel, ratio, multi=False, num_tasks=12):
+        super(Dcnn, self).__init__()
+
+        # Image (Cx64x64)
+        self.conv1 = BayesianConv2D(in_channels=input_channel, out_channels=16, kernel_size=7, stride=1, padding=3, ratio=ratio)
+        self.bn1 = nn.BatchNorm2d(num_features=16)
+
+        # State (16x32x32)
+        self.conv2 = BayesianConv2D(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2, ratio=ratio)
+        self.bn2 = nn.BatchNorm2d(num_features=32)
+
+        # State (32x16x16)
+
+        self.conv3 = BayesianConv2D(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1, ratio=ratio)
+        self.bn3 = nn.BatchNorm2d(num_features=16)
+
+        # State (16x8x8)
+
+        # State (1024x1x1)
+        self.fc1 = BayesianLinear(1024, 128, ratio=ratio)
+        self.drop1 = nn.Dropout(0.4)
+        # State (128)
+        self.fc2 = BayesianLinear(128, 128, ratio=ratio)
+        self.drop2 = nn.Dropout(0.4)
+
+        # State (128)
+        self.multi = multi
+        if self.multi:
+            self.num_tasks = num_tasks
+            self.last = nn.ModuleList()
+            for _ in range(self.num_tasks):
+                self.last.append(nn.Linear(128, 7))
+        else:
+            self.last = nn.Linear(128, 7)
+
+    def forward(self, x, head_idx=0):
+        x = F.max_pool2d(F.relu(self.bn1(self.conv1(x))))
+        x = F.max_pool2d(F.relu(self.bn2(self.conv2(x))))
+        x = F.max_pool2d(F.relu(self.bn3(self.conv3(x))))
+
+        x = x.reshape(x.size(0), -1)
+
+        x = self.drop1(F.relu(self.fc1(x)))
+        x = self.drop2(F.relu(self.fc2(x)))
+
+        if self.multi:
+            x = self.last[head_idx](x)
+        else:
+            x = self.last(x)
+
+        return x
+
+"""
 class Dcnn(nn.Module):
     def __init__(self, input_channel, ratio, multi=False, num_tasks=12):
         super(Dcnn, self).__init__()
@@ -62,3 +118,4 @@ class Dcnn(nn.Module):
             x = self.last(x)
 
         return x
+"""
