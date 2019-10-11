4c84c886ee7224f545d0b60dfd82e6ba2dc785b1
H.Park <hyeongminpark5@gmail.com>
2019-10-12 02:55:50 +0900

    log edit

--- utils.py
+++ utils.py
@@ -162,7 +162,10 @@ def make_log_name(args):
         log_name += '_replay_r{}_gp_lamb{}'.format(args.replay_r, args.gan_gp_lambda)
     elif args.icarl:
         log_name += '_icarl'
-        log_name += '_K{}_num_cls_per_task{}_feature_size{}'.format(args.icarl_K, args.icarl_num_cls_per_task, args.icarl_feature_size)
+        log_name += '_K{}_num_cls_per_task{}_feature_size{}_fixed_rep{}'.format(args.icarl_K,
+                                                                                args.icarl_num_cls_per_task,
+                                                                                args.icarl_feature_size,
+                                                                                args.icarl_fixd_rep)
     else:
         log_name += '_fine'
 
